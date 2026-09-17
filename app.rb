039d3c097cc0dcdc9ee8f689fdd402c7f4f595f6
kioriii <kiorionwe@gmail.com>
2026-09-18 02:45:02 +0800

    Finished working on palindrome detector.

--- app.rb
+++ app.rb
@@ -19,3 +19,9 @@ get '/palindrome' do
   @title = 'Palindrome Detector'
   erb :palindrome
 end
+
+# This code allows you to inspect the parameters of the site or code that was entered.
+post '/check' do
+  @phrase = params[:phrase]
+  erb :result
+end
